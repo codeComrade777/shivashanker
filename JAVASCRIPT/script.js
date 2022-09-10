b5eb641467c709b0d38740983d391f9c588d4f17
codeComrade <110293984+codeComrade777@users.noreply.github.com>
2022-09-10 18:58:53 +0530

    Update script.js

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,1 +1,17 @@
-console.log("h");
+const trigger=document.querySelector('#trigger');
+const trigger1=document.querySelector('#trigger1');
+const trigger2=document.querySelector('#trigger2');
+const container=document.querySelector('#model');
+const closeBtn=document.querySelector('#close');
+trigger.addEventListener("click",function(){
+    container.classList.add("active");
+});
+trigger1.addEventListener("click",function(){
+    container.classList.add("active");
+});
+trigger2.addEventListener("click",function(){
+    container.classList.add("active");
+});
+closeBtn.addEventListener("click",function(){
+    container.classList.remove("active");
+});
